5187769fa593e0dd8d9deffbe92fc7fe036a24d4
Tom Farmer <tfarmer7980@gmail.com>
2021-02-25 22:01:04 +0000

    GPIO Input Ouput Complete

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,14 @@ mod memory;
 
 fn kernel_init() -> ! {
 
-    const DELAY: usize = 500000;
-    const GPIO_BASE: usize = 0xFE20_0000;
+    let mut pin_16 = pi::drivers::gpio::GpioPin::new(16);
+    pin_16 = pin_16.into_output();
 
-    let GPFSEL1 = 0xFE20_0004 as *mut u32;
-    let GPSET0 = 0xFE20_001C as *mut u32;
-    let GPCLR0 = 0xFE20_0028 as *mut u32;
-
-    unsafe { GPFSEL1.write_volatile(0b001 << 18)};
 
     loop {
-        unsafe {GPSET0.write_volatile(1 << 16)};
+        pin_16.set();
         pi::drivers::timer::spin_sleep_ms(1000);
-        unsafe {GPCLR0.write_volatile(1 << 16)};
+        pin_16.clear();
         pi::drivers::timer::spin_sleep_ms(1000);
     };
 }
--- src/pi/drivers/gpio.rs
+++ src/pi/drivers/gpio.rs
@@ -2,111 +2,129 @@ use crate::pi::memory;
 use super::common::StaticRef;
 use tock_registers::{register_bitfields, register_structs};
 use tock_registers::registers::*;
+use core::marker::PhantomData;
+
+
+pub struct GpioPin<State> {
+    /// Far to lazy to create an enum to represent the pin number so it's a u32, probably too big :)
+    pin: u8,
+    registers: StaticRef<GpioRegisters>,
+    _state: PhantomData<State>,
+}
+
+/// GPIO States available
+pub enum Uninitialized {}
+pub enum Input {}
+pub enum Output {}
+pub enum Alt {}
+
+#[repr(u8)]
+pub enum Function {
+    Input = 0b000,
+    Output = 0b001,
+    Alt0 = 0b100,
+    Alt1 = 0b101,
+    Alt2 = 0b110,
+    Alt3 = 0b111,
+    Alt4 = 0b011,
+    Alt5 = 0b010
+}
+
+/// Complier will infer type of S via context of calling functions return type
+impl<T> GpioPin<T>{
+    pub fn transition<S>(self) -> GpioPin<S> {
+        GpioPin{
+            pin: self.pin,
+            registers: self.registers,
+            _state: PhantomData
+        }
+    }
+}
+
+impl<Uninitialized> GpioPin<Uninitialized> {
+    pub fn new(pin: u8) -> GpioPin<Uninitialized> {
+        GpioPin {
+            pin,
+            registers: unsafe { StaticRef::new(memory::map::GPIO_START as *const GpioRegisters) },
+            _state: PhantomData
+        }
+    }
+
+    pub fn into_alt(self, function: Function) -> GpioPin<Alt> {
+        let register = self.pin/10;
+        let pin = self.pin % 10;
+        let value = self.registers.gpfselx[register as usize].get();
+        self.registers.gpfselx[register as usize].set(value | ((function as u32) << (3 * pin)));
+        self.transition()
+    }
+
+    pub fn into_output(self) -> GpioPin<Output> {
+        self.into_alt(Function::Output).transition()
+    }
+
+    pub fn into_input(self) -> GpioPin<Input> {
+        self.into_alt(Function::Input).transition()
+    }
+
+}
+
+impl GpioPin<Output> {
+    pub fn set(&mut self) {
+        let register = self.pin/32;
+        let pin = self.pin % 32;
+        self.registers.gpsetx[register as usize].set(1 << pin);
+    }
+    pub fn clear(&mut self) {
+        let register = self.pin/32;
+        let pin = self.pin % 32;
+        self.registers.gpclrx[register as usize].set(1 << pin);
+    }
+}
+
+impl GpioPin<Input> {
+    pub fn level(&mut self) -> bool {
+        let register = self.pin/32;
+        let pin = self.pin % 32;
+        (self.registers.gplevx[register as usize].get() & 1 << pin) == 1 << pin
+    }
+}
+
+
+
+register_structs!{
+    GpioRegisters{
+        (0x000 => gpfselx:  [ReadWrite<u32, GPFSELX::Register>; 6]),
+        (0x018 => _r1),
+        (0x01c => gpsetx:   [ReadWrite<u32, GPSETX::Register>; 2]),
+        (0x024 => _r2),
+        (0x028 => gpclrx:   [ReadWrite<u32, GPCLRX::Register>; 2]),
+        (0x030 => _r3),
+        (0x034 => gplevx:   [ReadWrite<u32, GPLEVX::Register>; 2]),
+        (0x03c => _r4),        
+        (0x040 => gpedsx:   [ReadWrite<u32, GPEDSX::Register>; 2]),
+        (0x048 => _r5),
+        (0x04c => gprenx:   [ReadWrite<u32, GPRENX::Register>; 2]),
+        (0x054 => _r6),
+        (0x058 => gpfenx:   [ReadWrite<u32, GPFENX::Register>; 2]),
+        (0x060 => _r7),
+        (0x064 => gphenx:   [ReadWrite<u32, GPHENX::Register>; 2]),
+        (0x06c => _r8),
+        (0x070 => gplenx:   [ReadWrite<u32, GPLENX::Register>; 2]),
+        (0x078 => _r9),
+        (0x07c => gparenx:  [ReadWrite<u32, GPARENX::Register>; 2]),
+        (0x084 => _r10),
+        (0x088 => gpafenx:  [ReadWrite<u32, GPAFENX::Register>; 2]),
+        (0x090 => _r11),
+        (0x0e4 => gppupdx:  [ReadWrite<u32, GPPUPDX::Register>; 4]),
+        (0x164 => @END),
+    }
+}
 
 register_bitfields!{
     u32,
 
     GPFSELX [
-        FSEL0 OFFSET(0) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL1 OFFSET(3) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL2 OFFSET(6) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL3 OFFSET(9) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL4 OFFSET(12) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL5 OFFSET(15) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL6 OFFSET(18) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL7 OFFSET(21) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL8 OFFSET(24) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ],
-        FSEL9 OFFSET(27) NUMBITS(3) [
-            Input   = 0b000,
-            Output  = 0b001,
-            Alt0    = 0b100,
-            Alt1    = 0b101,
-            Alt2    = 0b110,
-            Alt3    = 0b111,
-            Alt4    = 0b011,
-            Alt5    = 0b010
-        ]
+        FSEL OFFSET(0) NUMBITS(32) []
     ],
     /// GPIO Pin Set Register
     GPSETX [
@@ -148,5 +166,112 @@ register_bitfields!{
     /// GPIO Low Level Detect enable register
     GPLENX [
         LEN OFFSET(0) NUMBITS(32) []
+    ],
+    /// GPIO Async rising edge detect enable register
+    GPARENX [
+        AREN OFFSET(0) NUMBITS(32) []
+    ],
+    /// GPIO Async falling edge detect enable register
+    GPAFENX [
+        AFEN OFFSET(0) NUMBITS(32) []
+    ],
+    /// GPIO Pull up Pull down Control Registers
+    GPPUPDX [
+        GPPUPD00 OFFSET(0) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD01 OFFSET(2) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD02 OFFSET(4) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD03 OFFSET(6) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD04 OFFSET(8) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD05 OFFSET(10) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD06 OFFSET(12) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD07 OFFSET(14) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD08 OFFSET(16) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD09 OFFSET(18) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD10 OFFSET(20) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD11 OFFSET(22) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD12 OFFSET(24) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD13 OFFSET(26) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD014 OFFSET(28) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ],
+        GPPUPD015 OFFSET(30) NUMBITS(2) [
+            NoResistor  = 0b00,
+            PullDown    = 0b01,
+            PullUp      = 0b10,
+            Reserved    = 0b11
+        ]
     ]
 }
