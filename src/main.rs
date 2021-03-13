977cce01391f1df1c8d2f5be799e3e27f384ad8b
Tom Farmer <tfarmer7980@gmail.com>
2021-03-13 14:26:58 +0000

    Uart Works - PC connection broke

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
 
 use core::fmt::Write;
 
+use pi::drivers::timer::spin_sleep_ms;
+
 mod pi;
 mod panic_wait;
 mod arch;
@@ -17,10 +19,37 @@ fn kernel_init() -> ! {
     let mut uart = pi::drivers::uart::MiniUart::new();
     uart.init();
 
+    let mut pin16 = pi::drivers::gpio::GpioPin::new(16).into_output();
 
     loop {
-        // uart.write_str("Hello").expect("No");
-        uart.write_str("Hello").unwrap();
-        pi::drivers::timer::spin_sleep_ms(100);
+        // let byte = 0x40;
+        // uart.write_byte(byte);
+        
+        // if uart.has_byte() {
+        //     let new = uart.read_byte();
+
+        //     if new == byte {
+        //         pin16.set();
+        //         spin_sleep_ms(100);
+        //         pin16.clear();
+        //         spin_sleep_ms(100);
+        //     }
+        //     else {
+        //         pin16.set();
+        //         spin_sleep_ms(100);
+        //         pin16.clear();
+        //         spin_sleep_ms(100);
+        //         pin16.set();
+        //         spin_sleep_ms(100);
+        //         pin16.clear();
+        //     }
+        // }        
+
+        // spin_sleep_ms(2000);
+        uart.write_byte(1);
+        uart.write_byte(2);
+        uart.write_byte(3);
+        uart.write_byte(4);
+        uart.write_byte(5);
     };
 }
